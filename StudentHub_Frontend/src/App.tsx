97c6c1f831441c80aa53d329c7a1c7148a3f3fe1
Revanth <revanthg2607@gmail.com>
2025-09-22 11:00:34 +0530

    Update package.json and App.tsx for mock tests feature

--- StudentHub_Frontend/src/App.tsx
+++ StudentHub_Frontend/src/App.tsx
@@ -5,7 +5,6 @@ import MainLayout from "./pages/HomePage/MainLayout";
 import HomePage from "./pages/HomePage/HomePage";
 import SearchPage from "./pages/SearchPage/SearchPage";
 import MockTests from "./pages/MockTests";
-import { AuthProvider } from "./contexts/AuthContext";
 
 // import Search from "./pages/Other/Search";
 // import Test from "./pages/Other/Test";
@@ -16,31 +15,29 @@ import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-50">
-          <Routes>
-            {/* Wrap routes that need Header in a layout */}
-            <Route element={<MainLayout />}>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/mock-tests" element={<MockTests />} />
-            </Route>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          {/* Wrap routes that need Header in a layout */}
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/mock-tests" element={<MockTests />} />
+          </Route>
 
-            <Route path="/search" element={<SearchPage />} />
-            {/* <Route path="/search-page" element={<Search />} /> */}
-            {/* <Route path="/test" element={<Test />} /> */}
-            {/* Other routes without header */}
-            {/* <Route path="/about" element={<About />} /> */}
-            {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/search" element={<SearchPage />} />
+          {/* <Route path="/search-page" element={<Search />} /> */}
+          {/* <Route path="/test" element={<Test />} /> */}
+          {/* Other routes without header */}
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="/contact" element={<Contact />} /> */}
 
-            {/* <Route path="/courses/*" element={<Courses />} /> */}
-            {/* <Route path="/exams/*" element={<Exams />} /> */}
-            {/* <Route path="/resources/*" element={<Resources />} /> */}
-            {/* <Route path="/login" element={<Login />} /> */}
-          </Routes>
-        </div>
-      </Router>
-    </AuthProvider>
+          {/* <Route path="/courses/*" element={<Courses />} /> */}
+          {/* <Route path="/exams/*" element={<Exams />} /> */}
+          {/* <Route path="/resources/*" element={<Resources />} /> */}
+          {/* <Route path="/login" element={<Login />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
